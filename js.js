acfe1e84c0b5f9ec0871d5d620e3cc497bb4cc98
Gabriel Luka de Amaral <gabrielluka@weg.net>
2023-08-21 21:52:48 -0300

    a

--- js.js
+++ js.js
@@ -4,6 +4,7 @@ let vida = 200;
 const barraVida = document.querySelector('#vida');
 let examplePlane = document.querySelector('#example-plane');
 console.log(barraVida.style.width);
+console.log(+barraVida.style.width - 10);
 examplePlane.addEventListener("click", event => {
     examplePlane = document.querySelector('#example-plane');
 vida-=10;
