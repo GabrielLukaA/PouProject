dfdf64ed1b3e79defc5d5d6bcf9bb73dd5af96b6
Gabriel Luka de Amaral <gabrielluka@weg.net>
2023-08-21 21:54:37 -0300

    vsf

--- js.js
+++ js.js
@@ -4,11 +4,12 @@ let vida = 200;
 const barraVida = document.querySelector('#vida');
 let examplePlane = document.querySelector('#example-plane');
 console.log(barraVida.style.width);
-console.log(+barraVida.style.width - 10);
+barraVida.style.width = ""+90+"px";
+console.log(barraVida.style.width);
 examplePlane.addEventListener("click", event => {
     examplePlane = document.querySelector('#example-plane');
 vida-=10;
-console.log(+barraVida.style.width - 10);
+
 
   if (vida==0){
     alert("Ganhamo")
