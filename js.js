37eda165fc93c6ef9b3ae8defadf70d46b9c73de
Gabriel Luka de Amaral <gabrielluka@weg.net>
2023-08-28 17:58:39 -0300

    opi

--- js.js
+++ js.js
@@ -7,20 +7,16 @@ let nivelNumber = 1;
 const barraVida = document.querySelector('#vida');
 let examplePlane = document.querySelector('#example-plane');
 const nivel = document.querySelector("#nivel")
-console.log(nivel.innerText)
-nivel.innerText = nivelNumber+1
-nivelNumber ++;
-
-console.log(nivel.innerText)
 
 examplePlane.addEventListener("click", event => {
     examplePlane = document.querySelector('#example-plane');
     vida -= dano;
     barraVida.style.width = "" + vida + "px";
-    if (vida == 0) {
+    if (vida <= 0) {
         dano = dano/100*75
         nivel.innerText = nivelNumber+1
         nivelNumber ++;
+        vida = 200;
         nivel.innerHTML(nivelNumber+1)
         alert("Parabéns! Passou de nível.")
     }
