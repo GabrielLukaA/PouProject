988c06735107057916fce2c86f5d6052b596b78e
Gabriel Luka de Amaral <gabrielluka@weg.net>
2023-08-28 18:27:45 -0300

    oi

--- js.js
+++ js.js
@@ -3,22 +3,78 @@ let vida = 200;
 let dano = 10;
 let nivelNumber = 1;
 
-
+var interval
+var sec = 30;
+const tempo = document.querySelector('#tempo');
 const barraVida = document.querySelector('#vida');
 let examplePlane = document.querySelector('#example-plane');
-const nivel = document.querySelector("#nivel")
+const nivel = document.querySelector("#nivel");
+const exampleTarget = document.querySelector("#manoelGomes")
+
+examplePlane.addEventListener("targetFound", event => {
+    interval = setInterval(aumenta, 1000);
+    barraVida.style.width = "" + vida + 'px'
+})
+
+
+
+function aumenta() {
+    sec--;
+    tempo.innerText = sec;
+    if (sec == 0) {
+        switch (nivelNumber) {
+            case 1:
+                alert("Mas já?")
+                break;
+            case 2:
+                alert("Não aguentou a pontada?")
+                break;
+            case 3:
+                alert("")
+                break;
+            case 4:
+                alert("")
+                break;
+            case 5:
+                alert("")
+                break;
+            case 6:
+                alert("")
+                break;
+            case 7:
+                alert("Você foi caneta azulado.")
+                break;
+            case 8:
+                alert("")
+                break;
+            case 9:
+                alert("Fraco.")
+                break;
+            case 10:
+                alert("30 segundos é o suficiente ela disse.")
+                break;
+        }
+
+    }
+}
+
+
+
 
 examplePlane.addEventListener("click", event => {
     examplePlane = document.querySelector('#example-plane');
     vida -= dano;
     barraVida.style.width = "" + vida + "px";
     if (vida <= 0) {
-        dano = dano/100*75
-        nivel.innerText = nivelNumber+1
-        nivelNumber ++;
+
+        dano = dano / 100 * 75
+        nivel.innerText = nivelNumber + 1
+        nivelNumber++;
         vida = 200;
-        nivel.innerHTML(nivelNumber+1)
+        nivel.innerHTML(nivelNumber + 1);
         alert("Parabéns! Passou de nível.")
+        sec = 30;
+        tempo.innerText = sec;
     }
 });
 
