df526fae171bdd8c2e23a279d3d6493b355959b5
Gabriel Luka de Amaral <gabrielluka@weg.net>
2023-08-28 17:57:06 -0300

    oi

--- js.js
+++ js.js
@@ -1,15 +1,28 @@
 
 let vida = 200;
+let dano = 10;
+let nivelNumber = 1;
+
 
 const barraVida = document.querySelector('#vida');
 let examplePlane = document.querySelector('#example-plane');
+const nivel = document.querySelector("#nivel")
+console.log(nivel.innerText)
+nivel.innerText = nivelNumber+1
+nivelNumber ++;
+
+console.log(nivel.innerText)
 
 examplePlane.addEventListener("click", event => {
     examplePlane = document.querySelector('#example-plane');
-    vida -= 10;
+    vida -= dano;
     barraVida.style.width = "" + vida + "px";
     if (vida == 0) {
-        alert("Ganhamo")
+        dano = dano/100*75
+        nivel.innerText = nivelNumber+1
+        nivelNumber ++;
+        nivel.innerHTML(nivelNumber+1)
+        alert("Parabéns! Passou de nível.")
     }
 });
 
